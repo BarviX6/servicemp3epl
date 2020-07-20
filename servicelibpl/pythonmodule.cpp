e50646f1e46f8b90eec035aefb15c71526883a22
BarviX6 <barvix6@gmail.com>
2020-07-20 22:56:04 +0300

    Update pythonmodule.cpp

--- servicelibpl/pythonmodule.cpp
+++ servicelibpl/pythonmodule.cpp
@@ -6,8 +6,28 @@ static PyMethodDef servicelibplMethods[] =
 	{NULL,NULL,0,NULL}
 };
 
+#if PY_MAJOR_VERSION >= 3
+	static struct PyModuleDef moduledef = {
+		PyModuleDef_HEAD_INIT,
+		"servicelibpl",					/* m_name */
+		"Module for servicelibpl",		/* m_doc */
+		-1,									/* m_size */
+		servicelibplMethods,			/* m_methods */
+		NULL,								/* m_reload */
+		NULL,								/* m_traverse */
+		NULL,								/* m_clear */
+		NULL,								/* m_free */
+	};
+
+PyMODINIT_FUNC PyInit_servicelibpl(void)
+{
+    return PyModule_Create(&moduledef);
+}
+#else
+
 PyMODINIT_FUNC
 initservicelibpl(void)
 {
 	Py_InitModule("servicelibpl", servicelibplMethods);
 }
+#endif
